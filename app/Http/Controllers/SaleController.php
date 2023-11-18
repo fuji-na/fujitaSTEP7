1daef89ceedec019eaa7cec3d86ff0acb237e44c
fuji-na <n.fujita726@gmail.com>
2023-11-18 23:14:03 +0900

    API実装11/18

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Sale;
 
 class SaleController extends Controller
 {
-    //
+    public function purchase(Request $request) {
+        //リクエストからデータを取得(必要なデータを理解しておく)
+        $productId = $request->input('product_id');//送られてきた"pruduct_id"を代入
+        $quantity = $request->input('quantity', 1);//購入する数を代入　"quantity"のデータが送られなければ1を代入
+
+        //データベースから商品の商品を検索、取得
+        $product = Product::find($productId);//"product_id"が送られてきたらProduct::find($productId)の情報が代入される
+
+        //商品が存在しない、在庫が不足しているときのバリデーション
+        if(!$product) {
+            return response()->json(['message' => '商品が存在しません'], 404);
+        }
+        if($quantity <= 0 || $product->stock < $quantity) {
+            return response()->json(['message' => '商品が在庫不足です'], 400);
+        }
+
+        //在庫を減算
+        $product->stock -= $quantity;
+        $product->save();
+
+        //Salesテーブルに商品IDと購入日時を記録
+        $sale = new Sale([
+            'product_id' => $productId,//配列にする
+        ]);
+
+        //レスポンスを返す
+        return response()->json(['message' => '購入成功']);
+    }
 }
