86d94234aa3a7732299f5c8e2fc60bdccb090579
fuji-na <n.fujita726@gmail.com>
2023-12-18 23:16:18 +0900

    1218

--- resources/js/app.js
+++ resources/js/app.js
@@ -42,35 +42,3 @@ $('deleteBtn{{$product->id}}').on('post', function() {
     });
 });
 
-//Ajaxが走らない。そもそも成功可否のアラートが表示されない→id指定がおかしい？
-
-
-//ajaxのdataとdataTypeの違いとは？
-/*$("#search"). on("submit", function() {
-    $.ajax({
-        type: 'GET',
-        url: "{{ route('search') }}",
-        dataType: "json",
-        success: function(response) {
-
-        }
-    })
-})
-
-
-$.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': '{{ csrf_token() }}'
-    }
-});
-
-$(function() {
-    $.ajax({
-        type: 'POST',
-        url: '/post.destroy/' + productId, 
-        dataType: "json",
-        success: function(response) {
-            
-        }
-    })
-})*/
