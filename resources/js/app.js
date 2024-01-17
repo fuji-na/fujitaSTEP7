9b7f2889da0573840f4551ea115529b33affe1b6
fuji-na <n.fujita726@gmail.com>
2024-01-17 14:53:54 +0900

    01/17ソート

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import './bootstrap';
 
 
-$("#search").on("submit", function(event) {
+$("#search").on("submit", function (event) {
     event.preventDefault();
-    var formData = $(this).serialize(); 
+    var formData = $(this).serialize();
 
     $.ajax({
         type: "GET",
@@ -13,18 +13,51 @@ $("#search").on("submit", function(event) {
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         },
-        success: function(data) {
-            console.log(data)
+        success: function (data) {
+            console.log(data);
             var newTableBody = $(data).find('table.all tbody');
             $("#product-list table.all tbody").html(newTableBody.html());
 
+            // ソートが有効になるようにsortableを適用
+            applySortable();
         },
-        error: function() {
+        error: function () {
             alert("読み込み失敗");
         }
     });
-
 });
+
+// ソートが有効になるように再度sortableを適用
+function applySortable() {
+    // @sortablelinkを再度有効にする
+    $(".sortable-column ").on("click", function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        sortTable(href);
+    });
+}
+
+// テーブルをソートする関数
+function sortTable(href) {
+    $.ajax({
+        type: "GET",
+        url: href,
+        dataType: "html",
+        success: function (data) {
+            console.log(data);
+            var newTableBody = $(data).find('table.all tbody');
+            $("#product-list table.all tbody").html(newTableBody.html());
+            applySortable(); // ソートを有効にする
+        },
+        error: function () {
+            alert("ソート失敗");
+        }
+    });
+}
+
+// ページロード時にsortableを初期化
+applySortable();
+
 
 
 //http://localhost:8888/FujitaSTEP7/public/ichiran
